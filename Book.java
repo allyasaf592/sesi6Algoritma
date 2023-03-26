9693aeaaa4c77eb6c992761e388edd49e57ad9b8
allyasaf592 <115513872+allyasaf592@users.noreply.github.com>
2023-03-26 22:29:58 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book {
+  String judul;
+  String kategori;
+  String penulis;
+  String penerbit;
+  int tahun;
+  
+  public Book(String judul, String kategori, String penulis, String penerbit, int tahun){
+    this.judul = judul;
+    this.kategori = kategori;
+    this.penulis = penulis;
+    this.penerbit = penerbit;
+    this.tahun = tahun;
+  }
+}
